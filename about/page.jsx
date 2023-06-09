161e4f332bebb2d7f996d063112b49e99f63eae9
jmauriciom <skinet021@gmail.com>
2023-06-09 16:04:25 -0300

    Firt Commit

---
+++ about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>
+    <h1>About Page</h1>
+    <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
+    voluptatibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
+    voluptatibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
+    voluptatibus?
+    </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ services/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ServicesPage() {
+  return (
+    <div>
+      <h1>ServicesPage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem?</p>
+    </div>
+  )
+}
+
+export default ServicesPage
