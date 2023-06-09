21905943e5ab8de665bdad0597b982c3f887ffa2
jmauriciom <skinet021@gmail.com>
2023-06-09 16:13:24 -0300

    second push

--- about/page.jsx
+++ about/page.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 function AboutPage() {
   return (
     <div>
-    <h1>About Page</h1>
-    <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
-    voluptatibus?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
-    voluptatibus?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
-    voluptatibus?
-    </p>
+      <h1>About Page</h1>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
+        voluptatibus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
+        voluptatibus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
+        voluptatibus?
+      </p>
+      <h5> hola github</h5>
     </div>
   )
 }
