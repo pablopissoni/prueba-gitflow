4cdf42cd35c78ed8f59bcf604e22c3c7760c4be0
pablopissoni <pablopissoni@gmail.com>
2023-06-09 17:35:54 -0300

    se agrego h1

--- about/page.jsx
+++ about/page.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function AboutPage() {
   return (
     <div>
+      <h2>Nuevo cambio desde rama fixpablo</h2>
     
       <h1>About Page Modificacion de prueba GIT</h1>
       <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
